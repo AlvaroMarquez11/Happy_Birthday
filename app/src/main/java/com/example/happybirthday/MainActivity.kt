71b9c2b95c5db03ee09d89d259dce627f5fb4b9c
AlvaroMarquez11 <130756201+AlvaroMarquez11@users.noreply.github.com>
2023-10-11 14:07:30 +0200

    Version final

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +34,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(message = "Happy Birthday Sam!", from ="From Emma")
+                    GreetingImage(
+                        "Happy birthday Emma", "From Alvaro"
+                    )
                 }
             }
         }
@@ -41,50 +44,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(mensaje: String, from: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    // Create a column so that texts don't overlap
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = modifier
     ) {
         Text(
-            text = mensaje,
+            text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 16.dp)
         )
         Text(
             text = from,
             fontSize = 36.sp,
             modifier = Modifier
-                .padding(16.dp)
+                .padding(top = 16.dp)
+                .padding(end = 16.dp)
                 .align(alignment = Alignment.End)
-        )
-    }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun BirthdayCardPreview() {
-    HappyBirthdayTheme {
-        GreetingImage(message = "Happy Birthday Sam!", from ="From Emma")
+        )
     }
 }
 
 @Composable
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.androidparty)
-    Box{
+    // Create a box to overlap image and texts
+    Box(modifier) {
         Image(
-            painter = image,
+            painter = painterResource(id = R.drawable.androidparty),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             alpha = 0.5F
         )
         GreetingText(
-            mensaje = message,
+            message = message,
             from = from,
-            modifier = Modifier.fillMaxSize().padding(8.dp)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
+}
 
+@Preview(showBackground = false)
+@Composable
+private fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingImage(
+            "Happy birthday Emma",
+            "From Alvaro"
+        )
+    }
 }
